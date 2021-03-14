11fd296035a64fb82908a83c7c4efccfbfbf0b47
ViktorJT <viktor.jensentorp@hyperisland.se>
2021-03-14 13:31:40 +0100

    Fixed build issue with contact page

--- gatsby/src/pages/contact.js
+++ gatsby/src/pages/contact.js
@@ -2,3 +2,7 @@ import React from 'react';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
 import Intro from '../components/Intro';
+
+export default function contact() {
+  return <div>Work in progress...</div>;
+}
